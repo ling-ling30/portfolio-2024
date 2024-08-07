9097cd1495f83b27cd78873bd88b3fe1a519418d
Ricky <ricky-inbox@outlook.com>
2024-08-07 16:34:02 +0700

    add skills page

---
+++ app/_components/skills/SkillMarquee.tsx
@@ -0,0 +1,109 @@
+import Marquee from "@/components/magicui/marquee";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  image: string;
+  label: string;
+};
+
+const data = [
+  {
+    image: "/authJS.png",
+    label: "AuthJS / NextAuth",
+  },
+  {
+    image: "/aws.svg",
+    label: "AWS",
+  },
+  {
+    image: "/css.svg",
+    label: "CSS",
+  },
+  {
+    image: "/docker.svg",
+    label: "Docker",
+  },
+  {
+    image: "/express.svg",
+    label: "Express",
+  },
+  {
+    image: "/js.svg",
+    label: "Javascript",
+  },
+  {
+    image: "/mongo.svg",
+    label: "MongoDB",
+  },
+  {
+    image: "/next-js.svg",
+    label: "NextJS",
+  },
+  {
+    image: "/prisma.svg",
+    label: "Prisma",
+  },
+  {
+    image: "/py.svg",
+    label: "Python",
+  },
+  {
+    image: "/nodejs.svg",
+    label: "NodeJS",
+  },
+  {
+    image: "/react.svg",
+    label: "React",
+  },
+  {
+    image: "/redis.svg",
+    label: "Redis",
+  },
+  {
+    image: "/sql.svg",
+    label: "Postgresql",
+  },
+  {
+    image: "/tailwind.svg",
+    label: "Tailwind",
+  },
+  {
+    image: "/typescript.svg",
+    label: "Typescript",
+  },
+  {
+    image: "/vercel.svg",
+    label: "Vercel",
+  },
+];
+
+const SkillCard = ({ image, label }: Props) => {
+  return (
+    <Card className="w-24 lg:w-40 bg-gradient-to-r from-violet-200 to-pink-200  dark:from-violet-800 dark:to-pink-800">
+      <CardHeader className="w-full flex items-center justify-center">
+        <div className="size-10 lg:size-20 flex items-center justify-center">
+          <Image className="" src={image} alt="" width={100} height={100} />
+        </div>
+      </CardHeader>
+      <CardContent className="w-full flex items-center justify-center pb-2">
+        <p className="text-sm lg:text-lg font-sans text-center font-bold">
+          {label}
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default function SkillMarquee() {
+  return (
+    <Marquee pauseOnHover className="[--duration:20s]">
+      {data.map((data) => {
+        return (
+          <SkillCard key={data.label} image={data.image} label={data.label} />
+        );
+      })}
+    </Marquee>
+  );
+}
---
+++ app/_components/skills/SkillStack.tsx
@@ -0,0 +1,27 @@
+import { anton } from "@/components/font";
+import Marquee from "@/components/magicui/marquee";
+import ShinyButton from "@/components/magicui/shiny-button";
+import { cn } from "@/lib/utils";
+import Spline from "@splinetool/react-spline";
+import React from "react";
+import SkillMarquee from "./SkillMarquee";
+import Game from "../Game/Game";
+
+type Props = {};
+
+export default function SkillStack({}: Props) {
+  return (
+    <div className="snap-start w-full h-screen bg-gradient-to-tr from-red-100 from-0% via-orange-100 to-yellow-100 dark:from-red-800 dark:from-0% dark:via-orange-800 dark:to-yellow-800">
+      <div
+        className={cn(
+          "w-full h-full flex flex-col items-center space-y-5",
+          `${anton.className}`
+        )}
+      >
+        <h1 className="text-4xl lg:text-6xl pt-5">Technical Skills.</h1>
+        <SkillMarquee />
+        <Game />
+      </div>
+    </div>
+  );
+}
