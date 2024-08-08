ce1a2206c4e940bad8b86d646c25dc7fb7f700cb
Ricky <ricky-inbox@outlook.com>
2024-08-08 11:30:36 +0700

    move loading provider

---
+++ components/providers/loading-provider.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Loader from "@/app/loader";
+import React, { useEffect, useState } from "react";
+
+type Props = { children: React.ReactNode };
+
+function LoadingProviders({ children }: Props) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const Interval = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      setIsLoading(false);
+    };
+
+    Interval();
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+  return <>{children}</>;
+}
+
+export default LoadingProviders;
