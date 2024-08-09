365ecd44fbb005b3a4804f720b96f89e2837b1db
Ricky - <rickyjay567@gmail.com>
2024-08-10 02:13:26 +0700

    remove unnesecerry dom

--- app/_components/Welcome.tsx
+++ app/_components/Welcome.tsx
@@ -9,26 +9,27 @@ type Props = {};
 
 export default function Welcome({}: Props) {
   return (
-    <>
-      <div className="relative flex justify-center items-center min-h-screen bg-gradient-to-b from-slate-100 from-0% via-gray-100 to-stone-100 dark:bg-gradient-to-t dark:from-stone-900 dark:to-gray-900 ">
-        <div className="">
-          <GradualSpacing
-            className={cn(
-              `${spaceMono.className}`,
-              "align-middle text-center text-2xl sm:text-4xl md:text-5xl lg:text-7xl xl:text-8xl font-bold tracking-[0.1em]  flex items-center bg-gradient-to-t from-blue-900 to bg-red-900 bg-clip-text text-transparent dark:bg-gradient-to-t dark:from-blue-200 dark:to-red-200 dark:bg-clip-text dark:text-transparent  md:leading-[5rem]"
-            )}
-            text=" Hi, I'm Ricky"
-            duration={0.5}
-            delayMultiple={0.1}
-          />
-          <LetterPullup
-            delay={0.1}
-            className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl bg-gradient-to-t from-blue-900 to bg-red-900 bg-clip-text text-transparent dark:bg-gradient-to-t dark:from-blue-200 dark:to-red-200 dark:bg-clip-text dark:text-transparent pt-10"
-            words="Full Stack Developer"
-          />
-        </div>
-        <BackgroundBeams />
+    <section
+      id="home"
+      className="relative flex snap-start justify-center items-center min-h-screen bg-gradient-to-b from-slate-100 from-0% via-gray-100 to-stone-100 dark:bg-gradient-to-t dark:from-stone-900 dark:to-gray-900 "
+    >
+      <div className="">
+        <GradualSpacing
+          className={cn(
+            `${spaceMono.className}`,
+            "align-middle text-center text-2xl sm:text-4xl md:text-5xl lg:text-7xl xl:text-8xl font-bold tracking-[0.1em]  flex items-center bg-gradient-to-t from-blue-900 to bg-red-900 bg-clip-text text-transparent dark:bg-gradient-to-t dark:from-blue-200 dark:to-red-200 dark:bg-clip-text dark:text-transparent  md:leading-[5rem]"
+          )}
+          text=" Hi, I'm Ricky"
+          duration={0.5}
+          delayMultiple={0.1}
+        />
+        <LetterPullup
+          delay={0.1}
+          className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl bg-gradient-to-t from-blue-900 to bg-red-900 bg-clip-text text-transparent dark:bg-gradient-to-t dark:from-blue-200 dark:to-red-200 dark:bg-clip-text dark:text-transparent pt-10"
+          words="Full Stack Developer"
+        />
       </div>
-    </>
+      <BackgroundBeams />
+    </section>
   );
 }
--- app/_components/experience/Experience.tsx
+++ app/_components/experience/Experience.tsx
@@ -9,54 +9,50 @@ function Experience({}: Props) {
   return (
     <div
       id="about"
-      className="relative snap-start w-full h-screen bg-gradient-to-bl from-cyan-100 from-0% via-sky-200 to-sky-300 dark:bg-gradient-to-b dark:from-cyan-900 dark:from-0% dark:via-sky-800 dark:to-sky-900"
+      className={cn(
+        "relative snap-start w-full h-screen bg-gradient-to-bl from-cyan-100 from-0% via-sky-200 to-sky-300 dark:bg-gradient-to-b dark:from-cyan-900 dark:from-0% dark:via-sky-800 dark:to-sky-900",
+        "w-full h-full flex flex-col items-center space-y-5",
+        `${anton.className}`
+      )}
     >
-      <div
+      <h1 className="text-6xl pt-5">ABOUT.</h1>
+      {/* introduction */}
+      <section
         className={cn(
-          "w-full h-full flex flex-col items-center space-y-5",
-          `${anton.className}`
+          `${spaceMono.className}`,
+          "mx-2 text-xs sm:text-md lg:text-lg p-4 max-w-[400px] sm:max-w-[600px] md:max-w-[800px] border border-white"
         )}
       >
-        <h1 className="text-6xl pt-5">ABOUT.</h1>
-        {/* introduction */}
-        <section
+        <p>
+          Hello! Since the beginning of 2023, I&apos;ve been deeply immersed in
+          coding, focusing on mastering the NEXT.js framework for frontend
+          development and Express.js for the backend. I constantly seek out best
+          practices to improve my code daily, primarily working with TypeScript.
+        </p>
+      </section>
+      {/* Experience */}
+      <h1 className="text-2xl">Experience</h1>
+      <section>
+        <article
           className={cn(
             `${spaceMono.className}`,
-            "mx-2 text-xs sm:text-md lg:text-lg p-4 max-w-[400px] sm:max-w-[600px] md:max-w-[800px] border border-white"
+            "mx-2 text-sm sm:text-md lg:text-lg p-4 min-w-[300px] sm:min-w-[500px] md:min-w-[600px] max-w-[400px] sm:max-w-[600px] md:max-w-[800px] border border-white"
           )}
         >
-          <p>
-            Hello! Since the beginning of 2023, I&apos;ve been deeply immersed
-            in coding, focusing on mastering the NEXT.js framework for frontend
-            development and Express.js for the backend. I constantly seek out
-            best practices to improve my code daily, primarily working with
-            TypeScript.
-          </p>
-        </section>
-        {/* Experience */}
-        <h1 className="text-2xl">Experience</h1>
-        <section>
-          <section
-            className={cn(
-              `${spaceMono.className}`,
-              "mx-2 text-sm sm:text-md lg:text-lg p-4 min-w-[300px] sm:min-w-[500px] md:min-w-[600px] max-w-[400px] sm:max-w-[600px] md:max-w-[800px] border border-white"
-            )}
+          <ExperienceItem
+            company="Anugerah Lestari"
+            date="November 2023 - now"
+            role="Full Stack Developer"
           >
-            <ExperienceItem
-              company="Anugerah Lestari"
-              date="November 2023 - now"
-              role="Full Stack Developer"
-            >
-              I worked in a team of two on building an ERP application that
-              covers inventory management, production, product management,
-              orders, purchasing, and accounting . The application utilizes a
-              microservice architecture. We used PostgreSQL as the database,
-              NEXT.js for the frontend, and Express.js for the backend. It is
-              deployed on AWS EC2 wrapped in Docker with S3 for file storage.
-            </ExperienceItem>
-          </section>
-        </section>
-      </div>
+            I worked in a team of two on building an ERP application that covers
+            inventory management, production, product management, orders,
+            purchasing, and accounting . The application utilizes a microservice
+            architecture. We used PostgreSQL as the database, NEXT.js for the
+            frontend, and Express.js for the backend. It is deployed on AWS EC2
+            wrapped in Docker with S3 for file storage.
+          </ExperienceItem>
+        </article>
+      </section>
     </div>
   );
 }
