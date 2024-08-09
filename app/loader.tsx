dbe37bcc9917925db525c70a6e563e44140185be
Ricky <ricky-inbox@outlook.com>
2024-08-09 10:51:54 +0700

    change loader

--- app/loader.tsx
+++ app/loader.tsx
@@ -1,14 +1,27 @@
 import NumberTicker from "@/components/magicui/number-ticker";
+import { MultiStepLoader } from "@/components/ui/multi-step-loader";
 import React from "react";
 
 type Props = {};
-
+const loadingStates = [
+  {
+    text: "Locating the ‘any’ key...",
+  },
+  {
+    text: "Summoning the hamsters to power the servers...",
+  },
+  {
+    text: "Loading... don't worry, we've got snacks!",
+  },
+];
 export default function Loader({}: Props) {
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center">
-      <p className="whitespace-pre-wrap text-8xl font-medium tracking-tighter text-black dark:text-white">
-        <NumberTicker value={100} />
-      </p>
+      <MultiStepLoader
+        loadingStates={loadingStates}
+        loading={true}
+        duration={680}
+      />
     </div>
   );
 }
